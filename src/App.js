a696ce59f643972bd540237da81643fa1d3696af
Ambani Matsedu <ambani.matsedu@gmail.com>
2020-01-27 20:39:06 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+//used in basename={process.env.PUBLIC_URL}
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Contacts from './components/contacts/Contacts';
+import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
+import Header from './components/layout/Header';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import Test from './components/test/Test';
+
+import { Provider } from './context';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <div>
+            <Header branding="Contact Manager"/>
+            <div className="container">
+             <Switch>
+               <Route exact path="/" component={Contacts}></Route>
+               <Route exact path="/contact/add" component={AddContact}></Route>
+               <Route exact path="/contact/edit/:id" component={EditContact}></Route>
+               {/* <Route exact path="/about/:id" component={About}></Route> */}
+               <Route exact path="/about" component={About}></Route>
+               <Route exact path="/test" component={Test}></Route>
+               <Route component={NotFound}></Route>
+             </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/test/Test.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+export default class Test extends Component {
+    state = {
+        title: '',
+        body: ''
+    }
+    componentDidMount() {
+        console.log('componentDidMount...');
+        fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(response => response.json())
+        .then(data =>this.setState({
+            title: data.title,
+            body: data.body
+        }))
+    }
+
+
+    // componentWillMount(){
+    //     console.log('componentWillMount...');
+    // }
+
+    // componentDidUpdate(){
+    //     console.log('componentDidUpdate...');
+    // }
+
+    // componentWillUpdate(){
+    //     console.log('componentWillUpdate...');
+    // }
+
+    // componentWillReceiveProps(nextProps, nextstate) {
+    //     console.log('componentWillReceiveProps...');
+    // }
+
+    // //DEPRICATED
+    // UNSAFE_componentWillReceiveProps(nextProps, nextstate) {
+    //     console.log('componentWillReceiveProps...');
+    // }
+
+    // static getDerivedStateFromProps(props, state) {
+    //     return null;
+    // }
+
+    // getSnapshotBeforeUpdate(prevProps, prevState){
+
+    // }
+
+    render() {
+        const {title, body } = this.state;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <p>{body}</p>
+            </div>
+        )
+    }
+}
