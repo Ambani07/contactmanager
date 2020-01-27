5c6b4c23d27ffb98ac98cbc62ca6aedc59b5209f
Ambani Matsedu <ambani.matsedu@gmail.com>
2020-01-27 20:43:29 +0200

    Updates

---
+++ precache-manifest.33a82a22ba8711fc24dbc5c81c0a1aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0db27581785b182adfecf79583a21b6",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ac40e793df7b1d99c7ce",
+    "url": "/contactmanager/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "91c7ca3b2d75393bb4b0",
+    "url": "/contactmanager/static/js/2.13ccbf6d.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.13ccbf6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac40e793df7b1d99c7ce",
+    "url": "/contactmanager/static/js/main.5de722a2.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
